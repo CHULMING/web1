555aae90e5225efceb6c6dbed175cbd7a870e150
CHULMING <43492779+CHULMING@users.noreply.github.com>
2019-04-29 00:23:27 +0900

    Add files via upload

---
+++ create.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset ="utf-8">
+	<title>WEB</title>
+</head>
+<body>
+	<h1>WEB</h1>
+	<ol>
+		<li>HTML</li>
+	</ol>
+
+	<form action="create_process.php" method="post">
+		<p><input type = "text" name = "title" placeholder="Title"></p>
+		<p><textarea name = "description" placeholder="dis"></textarea></p>
+		<p><input type = "submit"></p>
+
+
+</body>
+</html>
